2e40c613b325e41679cbecd52c23ed4030d780ba
Martin Benjamins <marlamin@marlamin.com>
2019-09-21 03:09:19 +0200

    Add sample code

---
+++ ItemSparseDump/Program.cs
@@ -0,0 +1,33 @@
+﻿using DBCD.Providers;
+using System;
+using System.IO;
+
+namespace ItemSparseDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbcd = new DBCD.DBCD(new ItemSparseProvider(), new GithubDBDProvider());
+            var ItemSparse = dbcd.Load("ItemSparse", "8.2.5.31921");
+            foreach (dynamic row in ItemSparse.Values)
+            {
+                Console.WriteLine(row.ID + " " + row.Display_lang);
+            }
+        }
+    }
+    class ItemSparseProvider : IDBCProvider
+    {
+        public Stream StreamForTableName(string tableName, string build)
+        {
+            if (File.Exists(tableName + ".db2"))
+            {
+                return File.Open(tableName + ".db2", FileMode.Open);
+            }
+            else
+            {
+                throw new FileNotFoundException("Could not find " + tableName);
+            }
+        }
+    }
+}
